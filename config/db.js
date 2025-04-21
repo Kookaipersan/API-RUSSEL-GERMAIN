65d2715e53846af7e6a6f45bd4dac1cafc497358
Kookaipersan <kookaipersan@gmail.com>
2025-04-21 20:46:42 +0200

    log mongo db

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Change `MONGO_URI` en `MONGODB_URI`
-    await mongoose.connect(process.env.MONGODB_URI); 
+    console.log("URI de MongoDB:", process.env.MONGODB_URI); // ← Ajout temporaire
+
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ MongoDB connecté avec succès');
   } catch (err) {
     console.error('❌ Erreur de connexion MongoDB:', err.message);
@@ -12,4 +13,5 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
 
